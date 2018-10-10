45f342d4db1b697534d31e71be10445bb64b32d4
Marta Bañón <mbanon@prompsit.com>
2018-10-10 13:18:44 +0200

    Threaded core

--- anonymizer.py
+++ anonymizer.py
@@ -13,3 +13,85 @@
 
 #We need to decide if we paralelize at lines/tus scope (in case of few files with a lot of lines)
 #or at document scope (in case of a lot of files with few lines) 
+
+
+import logging
+import traceback
+import sys 
+import argparse
+import os
+
+from tempfile import NamedTemporaryFile, gettempdir
+#from multiprocessing import cpu_count
+
+try:
+  from .util import logging_setup
+  from .anonymizer_core import anonymize
+  from .tmx_utils import tmx2paco
+except (ImportError, SystemError):
+  from util import logging_setup
+  from anonymizer_core import anonymize
+  from tmx_utils import tmx2text
+  
+  
+__author__ = "Marta Bañón"
+__version__ = "Version 0.1 # 05/10/2018 # Initial release # Marta Bañón"
+
+
+def initialization():
+  logging.info("Processing arguments...")
+  # Getting arguments and options with argparse
+  # Initialization of the argparse class
+  parser = argparse.ArgumentParser(prog=os.path.basename(sys.argv[0]), formatter_class=argparse.ArgumentDefaultsHelpFormatter, description=__doc__)
+  # Mandatory parameters
+  ## Input file (TMX). Try to open it to check if it exists
+  parser.add_argument('input', type=argparse.FileType('rt'), default=None, help="TMX file to be anonymized")
+  ## Output file (TMX). Try to open it to check if it exists
+  parser.add_argument('output', nargs='?', type=argparse.FileType('w'), default=sys.stdout, help="TMX file with anonymization annotations")
+  
+  # Options group
+  groupO = parser.add_argument_group('Optional')
+  groupO.add_argument('--tmp_dir', default=gettempdir(), help="Temporary directory where creating the temporary files of this program")
+#  groupO.add_argument('-b', '--block_size', type=int, default=10000, help="Sentence pairs per block")
+#  groupO.add_argument('-p', '--processes', type=int, default=max(1, cpu_count()-1), help="Number of processes to use")
+
+  # Logging group
+  groupL = parser.add_argument_group('Logging')
+  groupL.add_argument('-q', '--quiet', action='store_true', help='Silent logging mode')
+  groupL.add_argument('--debug', action='store_true', help='Debug logging mode')
+  groupL.add_argument('--logfile', type=argparse.FileType('a'), default=sys.stderr, help="Store log to a file")
+  groupL.add_argument('-v', '--version', action='version', version="%(prog)s " + __version__, help="show version of this script and exit")
+
+  # Validating & parsing
+  args = parser.parse_args()
+  logging_setup(args)
+  logging.debug("Arguments processed: {}".format(str(args)))
+  logging.info("Arguments processed.")
+  return args
+
+
+def main(args):
+    logging.info("Executing main program...")
+    srcsentences = NamedTemporaryFile(mode="w", delete=True, dir=args.tmp_dir)
+    trgsentences = NamedTemproaryFile(mode="w", delete=True, dir=args.tmp_dir)
+    scrlang = ""
+    trglang = ""
+    try:
+      tmx_utils.tmx2text(args.input, srcsentences, trgsentences, srclang, trglang)
+    except Exception as ex:
+      tb=traceback.format_exc()
+      print("Unable to extract text from TMX")
+      logging.error(tb)
+      sys.exit(1)
+    anonymizer_core.anonymize(args.output, srcsentences, trgsentences, srclang, trglang)
+    logging.info("Program finished")
+
+if __name__ == '__main__':
+    try:
+        logging_setup()
+        args = initialization() # Parsing parameters
+        main(args)  # Running main program
+    except Exception as ex:
+        tb = traceback.format_exc()
+        logging.error(tb)
+        sys.exit(1)
--- anonymizer_core.py
+++ anonymizer_core.py
@@ -5,3 +5,52 @@
 #Submodules run in parallel, and loaded as needed:
 #i.e.: eu-es will need one ixa and one bilst. es-en will need two instances of bilst 
 
+import multiprocessing as mp
+
+def extract(output, srcsentences, trgsentences, srclang, trglang):
+  output_queue = mp.Queue()  
+      
+  #0  
+  source_regex_proc = mp.Process(target=regex_module.extract, args=(srcsentences, output_queue))
+  #1
+  target_regex_proc = mp.Process(target=regex_module.extract, args=(trgsentences, output_queue))
+  
+  #2
+  source_addresses_proc = mp.Process(target=address_module.extract, args=(srcsentences, output_queue))
+  #3
+  target_addresses_proc = mp.Process(target=address_module.extract, args=(trgsentences, output_queue))
+  
+  source_names_module = selectNamesModule(srclang)
+  #4
+  source_names_proc = mp.Process(target=source_names_module.extract, args=(srcsentences, output_queue))
+  
+  target_names_module = selectNamesModule(trglang)
+  #5
+  target_names_proc = mp.Process(target=target_names_module.extract, args=(trgsentences, output_queue))
+  
+  processes = [source_regex_proc, target_regex_proc, source_addresses_proc, target_addresses_proc, source_names_proc, target_names_proc]
+  
+  for p in processes:
+    p.start()
+    
+  for p in processes:
+    p.join()
+    
+  results = [output_queue.get() for p in processes]
+  
+  source_regex_results = results[0]  
+  target_regex_results = results[1]
+  source_addresses_results = results[2]
+  target_addresses_results = results[3]
+  source_names_results = results[4]
+  target_names_results = results[5]
+  
+  processes.clear()
+  output_queue.clear()
+  
+  merge_proc = mp.Process(target=merger_module.merge, args=(source_regex_results, source_addresses_results, source_names_results, target_regex_results, target_addresses_results, target_names_results, target_output, merge_output))
+    
+  merge_proc.start()    
+  merge_proc.join()
+    
+  return
