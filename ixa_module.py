709523ebc3c7cffb9c4d564579564c267b1b3d5e
motagirl2 <mbanon@prompsit.com>
2018-11-08 19:08:02 +0100

    Added submodule ixa python

--- ixa_module.py
+++ ixa_module.py
@@ -3,12 +3,20 @@
 #ixa-module:  extracts name entities for EU, writes tempfile with numline, entities.
 import logging
 import entity
+import sys
+
+#TO DO
+sys.path.append("/home/motagirl2/projects/anonymizer/ixa-pipe-ner-python/ixa_pipes_python")
+
+import ixa_pipes_python.pipeline
+tagger=ixa_pipes_python.pipeline.IXANERPipeline('eu')
 
 def extract(sentence):
   
   entities = []
   
-  
+  tags = tagger.nertag(sentences)
+  process_tags(tags)
   return entities
 
  
