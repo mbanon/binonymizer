1c8ba184c9ebea54f90eb91422d375fface8b8f3
Marta Bañón <mbanon@prompsit.com>
2019-02-27 13:59:57 +0100

    Fixing submodule config

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt") as rf:
 
 setuptools.setup(
     name="binonymizer",
-    version="0.1.1",
+    version="0.1",
     install_requires=requirements,
     license="GNU General Public License v3.0",
     author="Prompsit Language Engineering",
